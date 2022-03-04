ee4823732688b82347a445bdfe4bd557633f73fd
emersonjunior8 <95448215+emersonjunior8@users.noreply.github.com>
2022-03-04 15:29:07 -0300

    Add files via upload

---
+++ Remove All Markers Regions.js
@@ -0,0 +1,16 @@
+/**
+* This script will remove all Markers, Regions, CD Indices, CD Tracks, and Command Markers
+*
+* By George Dean 05/27/2016 (with ideas from SonyPJM "Sony Vegas Pro Forums)
+*
+**/
+
+Vegas.Project.Markers.Clear();
+
+Vegas.Project.Regions.Clear();
+
+Vegas.Project.CDIndices.Clear();
+
+Vegas.Project.CDTracks.Clear();
+
+Vegas.Project.CommandMarkers.Clear();
